40718844c24372f368c7bc77a189c87bb8596d5a
Brad <brad@homesteadcabinet.net>
2024-01-09 16:08:19 -0700

    1-9-24

--- Code.js
+++ Code.js
@@ -1,77 +1,163 @@
 // import 'google-apps-script';
-const _ = LodashGS.load();
+
+// Fetch and pull data
+// clasp push
+// clasp pull
+
+
 const mv_data_sheet = "166LXg0Bmm2ea7t91FoE0mAEp_AFDBuPcbsM3oTfm9hQ";
 const bid_data_sheet = "1O85YYHYJivPOeAFWnIumpxYeH35qqhyvkN7_o0xbYfw";
 const currentSpreadsheet = SpreadsheetApp.getActiveSpreadsheet();
 var currentSheet = currentSpreadsheet.getActiveSheet();
+var settings = {};
 
+const departmentDataTemplate = {
+  status: '',
+  date_changed: '',
+  workload: 0,
+}
 
-function writeDataToTab(tab_name, data, header_row=true) {
-  let ss = SpreadsheetApp.openById(sheet_id);
-  let sheet = ss.getSheetByName(tab_name);
+function writeDataToTab(tab_name, data, header_row=false) {
+  let sheet = currentSpreadsheet.getSheetByName(tab_name);
+
+  // Check if sheet exists, create it if it doesn't
+  if (!sheet) {
+    sheet = currentSpreadsheet.insertSheet(tab_name);
+  }
+
+  let headers = Object.keys(Object.values(data)[0]);
+  let ddata = [];
   sheet.clearContents();
 
   if (!header_row) {
-    let headers = Object.keys(data[0]);
-    let ddata = [];
     ddata.push(headers);
-
-    for (let row of data) {
-      let d = _.values(row);
-      ddata.push(d);
-    }
-    data = ddata;
+  }
+  for (let row of data) {
+    let d = _.values(row);
+    ddata.push(d);
   }
 
-  let outputRange = sheet.getRange(1, 1, data.length, headers.length);
-  outputRange.setValues(data);
+  let outputRange = sheet.getRange(1, 1, ddata.length, headers.length);
+  outputRange.setValues(ddata);
 }
 
 
-function getDataFromSheet(sheet_id, tab_name, id_key="BidID") {
-  let rtn = {}
-  let ss = SpreadsheetApp.openById(sheet_id);
-  let sheet = ss.getSheetByName(tab_name);
-  let data = sheet.getDataRange().getValues();
+function getItem(item_id, items, id_key='id') {
+  return items.find(item => item[id_key] == item_id);
+}
 
-  // Extract the header row, this assumes there will always be a header row. If not, this will need to be modified
+
+function arrayToObject(data, id_key="id") {
+  let rtn = {};
   let headers = data.shift();
-
   for (let row of data) {
     let d = _.zipObject(headers, row);
-    try {
-      rtn[d[id_key]] = d;
-    } catch {
-      console.error("Error in row: ", d);
-      console.info("Check that the id_key is correct");
+    rtn[d[id_key]] = d;
+  }
+  return rtn;
+}
+
+function updateRecords(existing_items, new_items, id_key="id") {
+  let rtn = [];
+  for (let item of existing_items) {
+    let new_item = getItem(item[id_key], new_items, id_key);
+    if (new_item) {
+      rtn.push(new_item);
     }
   }
   return rtn;
 }
 
 
+function getDataFromExternalSheet(sheet_id, tab_name, additional_headers=[], sort_key=null) {
+  let rtn = [];
+  let ss = SpreadsheetApp.openById(sheet_id);
+  let sheet = ss.getSheetByName(tab_name);
+  let data = sheet.getDataRange().getValues();
+
+  // Extract the header row, this assumes there will always be a header row. If not, this will need to be modified
+  let headers = data.shift();
+  if (isSomething(additional_headers)) {
+    headers = headers.concat(additional_headers);
+  }
+  rtn = initData(data, headers, {
+    months: settings.previous_months,
+    date_key: sort_key
+  });
+  if (sort_key) {
+    rtn = sortBy(rtn, sort_key);
+  }
+
+  return rtn;
+}
+
+
+function getSettings() {
+  let sheet = currentSpreadsheet.getSheetByName("Settings");
+  let data = sheet.getRange("B5:C25").getValues();
+  console.log(data)
+
+  for (let row of data) {
+    if (isSomething(row[0])) {
+      let key = row[0];
+      let val = row[1];
+      settings[key] = val;
+    }
+  }
+}
+
 
 
 function getBidData() {
   var bids = {};
+  const additional_bid_headers = [
+    "WorkOrder", "purchasing", "cutout", "edgebanding", "drawer boxes", "specialty", "miter cell",
+    "box sorting", "maverick", "shipping (staging)", "shipping (loading)", "parts prep",
+    "paint bay", "assembly",
+  ];
+  getSettings();
 
-  wo_data = getDataFromSheet(mv_data_sheet, "WorkOrders", "LinkID");
-  room_data = getDataFromSheet(bid_data_sheet, "Room Data", "id");
-  bid_data = getDataFromSheet(bid_data_sheet, "Jobs", "id");
+  // Get existing data from the sheet
+  let sheet = currentSpreadsheet.getSheetByName("Jobs");
+  let existing_bids = sheet.getDataRange().getValues();
+
+  var bid_data = getDataFromExternalSheet(bid_data_sheet, "Jobs", additional_bid_headers, "-mtime");
+  if (isSomething(existing_bids)) {
+    bid_data = updateRecords(existing_bids, bid_data);
+  }
+
+
+  var wo_data = getDataFromExternalSheet(mv_data_sheet, "WorkOrders");
+  var room_data = getDataFromExternalSheet(bid_data_sheet, "Room Data");
 
   console.log("bid_data:", formatByteSize(roughSizeOfObject(bid_data)))
   console.log("wo_data:", formatByteSize(roughSizeOfObject(wo_data)))
-  console.log("room_data:", formatByteSize(roughSizeOfObject(room_data)), room_data)
+  console.log("room_data:", formatByteSize(roughSizeOfObject(room_data)))
 
-  for (let row of bid_data) {
-    let d = _.zipObject(headers, row);
-    if (d["id"] in wo_data) {
-      d["WorkOrder"] = wo_data[d["id"]]["Name"] || null;
-      d['status'] = wo_data[d["id"]]["Status"] || null;
+  bid_data.forEach(b => {
+    bid_work_orders = wo_data.filter(wo => wo["BidID"] == b["id"]);
+    if (bid_work_orders.length >= 1) {
+      b["WorkOrder"] = bid_work_orders[0]["Name"];
     }
-    bids[d["id"]] = d;
-  }
+  });
+
+  room_data.forEach(r => {
+    bid_work_orders = wo_data.filter(wo => wo["BidID"] == r["bid_id"]);
+    if (bid_work_orders.length >= 1) {
+      r["WorkOrder"] = bid_work_orders[0]["Name"];
+    }
+  });
 
   // Write bids to the sheet
-  writeDataToTab("Jobs", bids);
+  writeDataToTab("Jobs", bid_data);
+
+
+
+  submitted_bids = bid_data.filter(b => {
+    const submittedDate = new Date(b["submitted_date"]);
+    return submittedDate > oneYearAgo;
+  });
+
+  submitted_bids.sort((a, b) => new Date(b.submitted_date) - new Date(a.submitted_date));
+  writeDataToTab("SubmittedJobs", submitted_bids);
 }
--- Utils.js
+++ Utils.js
@@ -1,10 +1,17 @@
+const _ = LodashGS.load();
+const now = new Date();
+const oneYearAgo = new Date();
+oneYearAgo.setFullYear(oneYearAgo.getFullYear() - 1);
+
+
 
 function formatByteSize(bytes) {
   if(bytes < 1024) return bytes + " bytes";
   else if(bytes < 1048576) return Math.round((bytes / 1024), 3) + " KiB";
   else if(bytes < 1073741824) return Math.round((bytes / 1048576), 3) + " MiB";
   else return(bytes / 1073741824).toFixed(3) + " GiB";
-};
+}
+
 
 function roughSizeOfObject( value, level ) {
   function clear__visited__(value){
@@ -45,3 +52,81 @@ function roughSizeOfObject( value, level ) {
 }
 
 
+// Enhanched boolean function, checks for: empty strings, objects, arrays, null, undefined, etc.
+function isNothing(thing) {
+  if (isObject(thing)) {
+    return _.isEmpty(thing);
+  }
+  if (thing === undefined || thing === null || (isNaN(thing) && typeof thing == 'number')) {
+    return true;
+  } else {
+    if (thing.constructor === String || isArray(thing)) {
+      return !thing.length;
+    } else return false;
+  }
+}
+
+
+function isSomething(thing) {
+  return !isNothing(thing);
+}
+
+
+// Quick type identifier functions
+function isObject(thing) {
+  return (!!thing) && (thing.constructor === Object);
+}
+
+
+function isArray(thing) {
+  return (!!thing) && (thing.constructor === Array);
+}
+
+
+function isDecimal(number) {
+  return !!(number % 1);
+}
+
+
+function isIdentical(obj1, obj2) {
+  // Checks if two objects are identical
+  return _.isEqual(obj1, obj2);
+}
+
+
+function initData(
+    data, headers,
+    options={
+      date_key: 'mtime',
+      months: 12,
+    }
+  ) {
+  let rtn = [];
+  for (let row of data) {
+    let d = _.zipObject(headers, row);
+    if (options.months && options.date_key) {
+      if (options.date_key[0] == '-') options.date_key = options.date_key.slice(1);
+      console.log(d[options.date_key], options, new Date(d[options.date_key]) > new Date(now.getFullYear(), now.getMonth() - options.months, now.getDate()))
+      if (new Date(d[options.date_key]) > new Date(now.getFullYear(), now.getMonth() - options.months, now.getDate())) {
+        rtn.push(d);
+      }
+    } else rtn.push(d);
+  }
+  return rtn;
+}
+
+
+function sortBy(array, key) {
+  // Sorts an array of objects by the key provided
+  if (key) {
+    if (key[0] == "-") {
+      key = key.slice(1);
+      array.sort((a, b) => {
+        return new Date(b[key]) - new Date(a[key]);
+      });
+    } else {
+      array.sort((a, b) => new Date(a[key]) - new Date(b[key]));
+    }
+  }
+  return array;
+}
